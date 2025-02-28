ea81c0200107351e5611f7842fc6315f78b181f2
j-mlo <joanna.r.davis@outlook.com>
2025-02-28 02:30:13 +0000

    Set up structure of game functions in js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runTheGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calulateorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
